930b2cfe5bb392b6e6928f84ab4caae7f82996ca
Jake Davies <jgdavies24@gmail.com>
2018-06-04 10:48:40 -0700

    prefixes hostnames with cg- and api- unless localhost

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,20 @@
+const location = window.location;
+
+function buildPrefixedApiUri(prefix) {
+  return (
+    location.protocol +
+    '//' +
+    prefix +
+    location.hostname +
+    `${location.port ? ':${location.port}' : ''}`
+  );
+}
+
 const env = {
-  API_BASE: process.env.REACT_APP_API_BASE,
-  CG_API_BASE: process.env.REACT_APP_CG_API_BASE,
+  API_BASE:
+    location.hostname === 'localhost' ? 'http://localhost:8080' : buildPrefixedApiUri('cg-'),
+  CG_API_BASE:
+    location.hostname === 'localhost' ? 'http://localhost:8082' : buildPrefixedApiUri('api-'),
   CG_API_KEY: process.env.REACT_APP_CG_API_KEY
 };
 
