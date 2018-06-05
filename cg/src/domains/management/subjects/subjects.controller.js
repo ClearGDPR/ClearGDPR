eec06928de466e1bfa0f8705304e1e2ec53d1e1d
sindelio <sindelio@gmail.com>
2018-06-05 15:51:40 -0300

    More progress in pagination

--- cg/src/domains/management/subjects/subjects.controller.js
+++ cg/src/domains/management/subjects/subjects.controller.js
@@ -1,48 +1,18 @@
 const SubjectsService = require('./subjects.service');
 
-const PAGINATION_COUNT = 2; // HARD CODED CONSTANT
-
 class SubjectsController {
   constructor(subjectsService = null) {
     this.subjectsService = subjectsService || new SubjectsService();
   }
 
   async listSubjects(req, res) {
-    const subjectsList = await this.subjectsService.listSubjects();
-    var requestedPage = req.query.page;
-    var pages = [];
-
-    if (requestedPage === undefined) {
-      requestedPage = 1;
-    }
-
-    for (var i = 0; i * PAGINATION_COUNT < subjectsList.length; i++) {
-      pages[i] = subjectsList.slice(i * PAGINATION_COUNT, (i + 1) * PAGINATION_COUNT);
-    }
-
-    if (i == 0) {
-      // This block handles the case in which there are no subjects in the db
-      if (requestedPage == 1) {
-        return res.json({
-          pages: 1,
-          requestedPage,
-          subjects: [] //Empty page
-        });
-      }
-      if (requestedPage != 1) {
-        return res.json({ error: `page number too big, maximum page number is 1` });
-      }
-    }
-
-    if (requestedPage > i) {
-      return res.json({ error: `page number too big, maximum page number is ${i}` });
-    }
-
-    res.json({
-      pages: i,
-      requestedPage,
-      subjects: pages[requestedPage - 1]
-    });
+    const subjectsPage = await this.subjectsService.listSubjects(req.query.page);
+    res.json(subjectsPage);
+    // res.json({
+    //   pages: i,
+    //   requestedPage,
+    //   subjects: pages[requestedPage - 1]
+    // });
   }
 }
 
--- cg/src/domains/management/subjects/subjects.service.js
+++ cg/src/domains/management/subjects/subjects.service.js
@@ -2,19 +2,23 @@ const { db } = require('../../../db');
 const { decryptFromStorage } = require('../../../utils/encryption');
 const winston = require('winston');
 
+const PAGE_SIZE = 2; // This could go in constants, inside utils
+
 class SubjectsService {
   constructor(database = db) {
     this.db = database;
   }
 
-  async listSubjects() {
+  async listSubjects(requestedPage) {
     const data = await this.db('subjects')
       .join('subject_keys', 'subjects.id', '=', 'subject_keys.subject_id')
       .select('personal_data')
       .whereNotNull('personal_data')
       .select('key')
       .whereNotNull('key')
-      .orderBy('id', 'desc');
+      .orderBy('id', 'desc')
+      .limit(PAGE_SIZE)
+      .offset((requestedPage - 1) * PAGE_SIZE);
 
     const decryptedData = data
       .map(item => {
@@ -27,8 +31,8 @@ class SubjectsService {
         }
       })
       .filter(item => item !== null);
-    // After filtering there will be some entries with { test:true } from other tests in the decryptedData
 
+    console.log(decryptedData);
     return decryptedData;
   }
 }
