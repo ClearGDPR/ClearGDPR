cfc3d25fa738b70bda93bd95fd450c2ae747f092
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2018-06-05 16:59:12 +0200

    Fixing missing knexfile on CI

--- cg/knex-config-factory.js
+++ cg/knex-config-factory.js
@@ -1,22 +1,8 @@
+const knexfile = require('./knexfile');
+
 module.exports = function() {
   const dbName = global.dbName || process.env.DB_DATABASE;
+  knexfile.connection.database = dbName;
 
-  return {
-    client: process.env.DB_ENGINE,
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      port: +process.env.DB_PORT,
-      password: process.env.DB_PASSWORD,
-      database: dbName,
-      charset: 'utf8'
-    },
-    pool: {
-      min: +process.env.DB_POOL_MIN,
-      max: +process.env.DB_POOL_MAX
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  };
+  return knexfile;
 };
