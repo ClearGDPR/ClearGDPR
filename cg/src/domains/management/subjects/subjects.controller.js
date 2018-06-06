49d8f2afb26a1276dc91a9eaba2c8cb0d946b745
sindelio <sindelio@gmail.com>
2018-06-06 16:00:42 -0300

    Add cover for subjects personal data in /api/management/subjects/list endpoint

--- cg/src/domains/management/subjects/subjects.controller.js
+++ cg/src/domains/management/subjects/subjects.controller.js
@@ -1,4 +1,15 @@
 const SubjectsService = require('./subjects.service');
+const _ = require('underscore');
+
+function coverString(string) {
+  var firstCharacter = string[0];
+  var lastCharacter = string[string.length - 1];
+  var coveredString = new Array(string.length).fill('*');
+  coveredString[0] = firstCharacter;
+  coveredString[string.length - 1] = lastCharacter;
+  coveredString = coveredString.join('');
+  return coveredString;
+}
 
 class SubjectsController {
   constructor(subjectsService = null) {
@@ -7,7 +18,13 @@ class SubjectsController {
 
   async listSubjects(req, res) {
     const subjectsPage = await this.subjectsService.listSubjects(req.query.page);
-    res.json(subjectsPage);
+    subjectsPage.map(subject => {
+      // Covers the subjects data with '*'s
+      _.mapObject(subject, (value, key) => {
+        subject[key] = coverString(value);
+      });
+    });
+    return res.json(subjectsPage);
   }
 }
 
