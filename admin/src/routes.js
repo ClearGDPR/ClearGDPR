fd333ab4ec727b264115c35403d459aa4b3db869
Francisco Caballero <fjcero@gmail.com>
2018-06-12 21:42:40 -0300

    Add dashboard to new routes architecture

--- admin/src/routes.js
+++ admin/src/routes.js
@@ -3,6 +3,7 @@ import { Router, Route, Switch } from 'react-router-dom';
 
 // import App from './containers/App';
 import Login from './containers/Login/Login';
+import Dashboard from './components/core/pages/Dashboard';
 import NoMatch from './components/NoMatch';
 import history from './history';
 
@@ -10,6 +11,7 @@ export default () => (
   <Router history={history}>
     <Switch>
       <Route path="/login" component={Login} exact />
+      <Route path="/dashboard" component={Dashboard} />
       <Route component={NoMatch} />
     </Switch>
   </Router>
