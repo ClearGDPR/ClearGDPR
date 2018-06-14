ee0cce32c551cf33f7bfbdceb3e42dec86ee2cea
Jake Davies <jgdavies24@gmail.com>
2018-06-14 10:04:30 -0700

    test site build and travis config, added website

--- travis/deploy-static-sites.sh
+++ travis/deploy-static-sites.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 mkdir -p .docs
 tar cf - --exclude='./node_modules' **/*.md | (cd .docs; tar xf -)
+tar cf - --exclude='./node_modules' **/*.png | (cd .docs; tar xf -)
 tar cf - --exclude='./node_modules' *.md | (cd .docs; tar xf -)
+tar cf - --exclude='./node_modules' *.png | (cd .docs; tar xf -)
 cp -r docs ./.docs/docs
 cp *.png ./.docs/
 cp index.html ./.docs/
