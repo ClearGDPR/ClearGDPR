e832abffff6c70e432e6d037ce4bde9a7a0b4824
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2018-06-14 19:19:08 +0200

    Added redirect handling

--- admin/src/containers/Login/Login.js
+++ admin/src/containers/Login/Login.js
@@ -1,36 +1,49 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 import logo from '../../assets/logo.png';
 import Login from '../../components/Login/Login';
 import session from '../../helpers/Session';
 import config from '../../config';
 import '../../theme/Login.css';
 
-const handleLogin = (username, password) => {
-  fetch(`${config.API_URL}/api/management/users/login`, {
-    method: 'POST',
-    body: JSON.stringify({
-      username,
-      password
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(async res => {
-      session.set(await res.json());
-    })
-    .catch(err => {
-      return err;
-    });
-};
+const LoginContainer = withRouter(props => {
+  const { from } = props.location.state || { from: { pathname: '/' } };
+  const { history } = props;
 
-const LoginContainer = () => (
-  <section className="login-section">
-    <div className="login-card">
-      <img className="logo" src={logo} alt="Clear logo" />
-      <Login auth={handleLogin} />
-    </div>
-  </section>
-);
+  const handleLogin = (username, password) => {
+    fetch(`${config.API_URL}/api/management/users/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(async res => {
+        session.set(await res.json());
+        history.push(from);
+      })
+      .catch(err => {
+        return err;
+      });
+  };
+
+  return (
+    <section className="login-section">
+      <div className="login-card">
+        <img className="logo" src={logo} alt="Clear logo" />
+        <Login auth={handleLogin} />
+      </div>
+    </section>
+  );
+});
+
+LoginContainer.propTypes = {
+  location: PropTypes.object
+};
 
 export default LoginContainer;
