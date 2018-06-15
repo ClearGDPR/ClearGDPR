0a012e087fd3297afe3ef20dff332378a67a9699
Francisco Caballero <fjcero@gmail.com>
2018-06-15 12:43:43 -0300

    Updated dependencies. Added setup to jest.

--- admin/src/components/Login/Login.test.js
+++ admin/src/components/Login/Login.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Login from './Login';
-
-enzyme.configure({ adapter: new Adapter() });
 
 describe('(Component) Login', () => {
   it('should render correctly', () => {
