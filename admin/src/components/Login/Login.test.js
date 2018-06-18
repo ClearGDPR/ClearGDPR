8e71d76e0310235ccb5d29e81227176a0238d9f0
Francisco Caballero <fjcero@gmail.com>
2018-06-18 11:16:48 -0300

    Updated tests with snapshots

--- admin/src/components/Login/Login.test.js
+++ admin/src/components/Login/Login.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Login from './Login';
 
+const props = {
+  auth: jest.fn()
+};
+
 describe('(Component) Login', () => {
   it('should render correctly', () => {
-    const output = shallow(<Login />);
-    expect(output).toMatchSnapshot();
+    const component = shallow(<Login {...props} />);
+    expect(component).toMatchSnapshot();
   });
 });
