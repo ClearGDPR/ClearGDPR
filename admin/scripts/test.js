7c28108aaedb6e5ea9d26282df509e3028e031f7
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2018-06-18 19:43:50 +0200

    By default running tests without watch, but added a watch task in yarn as well

--- admin/scripts/test.js
+++ admin/scripts/test.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
@@ -17,11 +15,5 @@ require('../config/env');
 
 const jest = require('jest');
 let argv = process.argv.slice(2);
-
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
-}
-
 
 jest.run(argv);
