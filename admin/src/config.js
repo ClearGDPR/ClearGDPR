be0d7f72e3448c491036b24507d31e389dc0ae2a
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2018-06-20 16:44:06 +0200

    Added code to handle URLs to API when deployed

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,17 +1,27 @@
 const location = window.location;
 
 function buildPrefixedApiUri(prefix) {
+  let parts = location.hostname.split('.');
+  if (parts.length > 2) {
+    parts.shift();
+  }
+
+  const hostname = parts.join('.');
+
   return (
     location.protocol +
     '//' +
     prefix +
-    location.hostname +
+    '.' +
+    hostname +
     `${location.port ? `:${location.port}` : ''}`
   );
 }
 
 const config = {
-  API_URL: location.hostname === 'localhost' ? 'http://localhost:8082' : buildPrefixedApiUri('cg-')
+  // TODO: handle some way of configuring the API URL
+  API_URL:
+    location.hostname === 'localhost' ? 'http://localhost:8082' : buildPrefixedApiUri('cg-demo')
 };
 
 export default config;
