4f6c16c66dcdb1aa264496adf999ad5be74a47ba
Francisco Caballero <fjcero@gmail.com>
2018-06-21 10:43:19 -0300

    Updated Login container testing

--- admin/src/containers/Login/Login.test.js
+++ admin/src/containers/Login/Login.test.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
 import toJson from 'enzyme-to-json';
-import LoginContainer from './Login';
+import { createMemoryHistory } from 'history';
+
+import { LoginContainer } from './Login';
+// import session from '../../helpers/Session';
+
+jest.mock('../../helpers/Session');
 
 const flushPromises = () => new Promise(resolve => setImmediate(resolve));
 
 const setup = propOverrides => {
   const props = Object.assign({}, propOverrides);
-
+  const history = createMemoryHistory('/');
   const component = shallow(
-    <MemoryRouter initialEntries={[{ pathname: '/', key: 'testKey' }]}>
-      <LoginContainer location={{ from: { path: '/' } }} />
-    </MemoryRouter>
+    <LoginContainer location={{ from: { path: '/' } }} history={history} />
   );
 
   return { props, component };
@@ -27,24 +29,38 @@ describe('(Container) Login', () => {
   it('should send username and password and receive a token', async () => {
     global.fetch = jest.fn().mockImplementationOnce(() =>
       Promise.resolve({
+        ok: true,
         status: 200,
-        json: () => true
+        json: () => 'token'
       })
     );
 
-    // const { component } = setup();
+    const { component } = setup();
+    const instance = component.instance();
+    const response = await instance.handleLogin(null, null);
+
     await flushPromises();
+    expect(global.fetch).toBeCalled();
+    expect(response.status).toBe(200);
+    expect(response.json()).toBe('token');
   });
 
   it('should send bad username or password and fail', async () => {
     global.fetch = jest.fn().mockImplementationOnce(() =>
       Promise.resolve({
+        ok: false,
         status: 400,
         json: () => false
       })
     );
 
-    // const { component } = setup();
+    const { component } = setup();
+    const instance = component.instance();
+    const response = await instance.handleLogin(null, null);
+
     await flushPromises();
+    expect(global.fetch).toBeCalled();
+    expect(response.status).toBe(400);
+    expect(response.json()).toBe(false);
   });
 });
