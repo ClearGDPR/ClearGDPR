247b32e0574b38f1a7c6cfb7174db37daad5720c
Francisco Caballero <fjcero@gmail.com>
2018-06-21 12:24:40 -0300

    Reversed api urls prefixes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -12,9 +12,9 @@ function buildPrefixedApiUri(prefix) {
 
 const env = {
   API_BASE:
-    location.hostname === 'localhost' ? 'http://localhost:8080' : buildPrefixedApiUri('cg-'),
+    location.hostname === 'localhost' ? 'http://localhost:8080' : buildPrefixedApiUri('api-'),
   CG_API_BASE:
-    location.hostname === 'localhost' ? 'http://localhost:8082' : buildPrefixedApiUri('api-'),
+    location.hostname === 'localhost' ? 'http://localhost:8082' : buildPrefixedApiUri('cg-'),
   CG_API_KEY: 'notusedyet'
 };
 
