efeb0d4ae3c5f3630ab0e272847fb06e31945a8d
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2018-06-26 22:43:58 +0200

    Fixed the form component demo

--- admin/src/components/core/cards/dashboard/Form.js
+++ admin/src/components/core/cards/dashboard/Form.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import TextInput from './TextInput';
+import { Form as FormComponent } from 'react-form';
 
 const Form = () => {
   return (
-    <form action="" method="">
-      <TextInput for="example" label="Example" placeholder="This is an input example" />
-      <TextInput
-        for="example-error"
-        label="Example w/ error"
-        placeholder="This is an input example with error"
-        error
-        errorMessage="This is a medium length error message"
-      />
-      <fieldset>
-        <legend>Radio Buttons</legend>
-        <div className="input-group">
-          <input type="radio" id="radio-example-1" name="radio-example" value="1" />
-          <label htmlFor="radio-example-1">Radio Example 1</label>
-        </div>
-        <div className="input-group">
-          <input type="radio" id="radio-example-2" name="radio-example" value="2" />
-          <label htmlFor="radio-example-2">Radio Example 2</label>
-        </div>
-      </fieldset>
+    <FormComponent>
+      <form action="" method="">
+        <TextInput
+          for="example"
+          label="Example"
+          placeholder="This is an input example"
+          field="test1"
+        />
+        <TextInput
+          for="example-error"
+          label="Example w/ error"
+          placeholder="This is an input example with error"
+          error="This is a medium length error message"
+          field="test1"
+        />
+        <fieldset>
+          <legend>Radio Buttons</legend>
+          <div className="input-group">
+            <input type="radio" id="radio-example-1" name="radio-example" value="1" />
+            <label htmlFor="radio-example-1">Radio Example 1</label>
+          </div>
+          <div className="input-group">
+            <input type="radio" id="radio-example-2" name="radio-example" value="2" />
+            <label htmlFor="radio-example-2">Radio Example 2</label>
+          </div>
+        </fieldset>
 
-      <div className="input-group">
-        <input type="checkbox" id="checkbox-example" name="checkbox-example" />
-        <label htmlFor="checkbox-example">I'm a checkbox!</label>
-      </div>
+        <div className="input-group">
+          <input type="checkbox" id="checkbox-example" name="checkbox-example" />
+          <label htmlFor="checkbox-example">I'm a checkbox!</label>
+        </div>
 
-      <div>
-        <input type="submit" className="btn" value="submit example" />
-      </div>
-    </form>
+        <div>
+          <input type="submit" className="btn" value="submit example" />
+        </div>
+      </form>
+    </FormComponent>
   );
 };
 
