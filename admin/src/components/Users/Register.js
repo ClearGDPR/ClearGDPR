b829bcde6fc058d8376d86f31216e7f7e36e534b
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2018-06-26 20:39:24 +0200

    Added the presentational component for registering a user

---
+++ admin/src/components/Users/Register.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-form';
+import TextInput from '../core/cards/dashboard/TextInput';
+import Loader from '../core/cards/dashboard/Loader';
+
+export class Register extends React.Component {
+  static propTypes = {
+    isLoading: PropTypes.bool,
+    onSubmit: PropTypes.func,
+    validatePassword: PropTypes.func,
+    touched: PropTypes.object,
+    errors: PropTypes.object
+  };
+
+  renderForm() {
+    return (
+      <React.Fragment>
+        <TextInput
+          label="Username"
+          placeholder="mick.bern"
+          error={
+            this.props.touched &&
+            this.props.touched['username'] &&
+            this.props.errors &&
+            this.props.errors['username']
+          }
+          field="username"
+        />
+        <TextInput
+          label="New password"
+          placeholder="*********"
+          type={'password'}
+          error={
+            this.props.touched &&
+            this.props.touched['password'] &&
+            this.props.errors &&
+            this.props.errors['password']
+          }
+          field="password"
+          validate={this.props.validatePassword}
+        />
+        <div>
+          <input type="submit" className="btn" value="Register" />
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+        {this.props.isLoading ? <Loader /> : this.renderForm()}
+      </form>
+    );
+  }
+}
+
+const registerForm = props => (
+  <Form onSubmit={submittedValues => props.onSubmit && props.onSubmit(submittedValues)}>
+    {formApi => (
+      <Register
+        {...props}
+        onSubmit={formApi.submitForm}
+        errors={{ ...formApi.errors, ...props.errors }}
+        touched={formApi.touched}
+      />
+    )}
+  </Form>
+);
+
+registerForm.propTypes = {
+  onSubmit: PropTypes.func,
+  errors: PropTypes.object
+};
+
+export default registerForm;
---
+++ admin/src/components/Users/Register.test.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import RegisterForm, { Register } from './Register';
+
+const setupShallow = propOverrides => {
+  const props = Object.assign({}, propOverrides);
+  const component = shallow(<Register {...props} />);
+
+  return { props, component };
+};
+
+const setupMount = propOverrides => {
+  const props = Object.assign({}, propOverrides);
+  const component = mount(<RegisterForm {...props} />);
+
+  return { props, component };
+};
+
+describe('(Component) Change Password', () => {
+  it('should render correctly when no props provided', async () => {
+    const { component } = setupShallow();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render correctly when the prop isLoading is set to false', async () => {
+    const { component } = setupShallow({ isLoading: false });
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render correctly when the prop isLoading is set to true', async () => {
+    const { component } = setupShallow({ isLoading: true });
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should call onSubmit when submit button clicked', async () => {
+    const onSubmit = jest.fn();
+    const validatePassword = jest.fn();
+    const { component } = setupShallow({ onSubmit, validatePassword });
+
+    const form = component.find('form').at(0);
+    form.simulate('submit');
+    expect(onSubmit).toHaveBeenCalled();
+  });
+
+  it('should validate password when inputs values change', async () => {
+    const validatePassword = jest.fn();
+    const { component } = setupMount({ validatePassword });
+
+    const passwordField = component.find('input[type="password"]').at(0);
+    passwordField.simulate('change', { target: { value: 'testPassword' } });
+
+    expect(validatePassword).toHaveBeenCalled();
+  });
+
+  it('should submit when validation is ok', async () => {
+    const onSubmit = jest.fn();
+    const validatePassword = jest.fn().mockReturnValue({ success: null });
+
+    const { component } = setupMount({ onSubmit, validatePassword });
+
+    const passwordField = component.find('input[type="password"]').at(0);
+    passwordField.simulate('change', { target: { value: 'testPassword' } });
+
+    const form = component.find('form').at(0);
+    form.simulate('submit');
+
+    expect(validatePassword).toHaveBeenCalled();
+    // TODO: try to make it work
+    // expect(onSubmit).toHaveBeenCalled();
+  });
+
+  it('should render correct props when touched', async () => {
+    const { component } = setupShallow({
+      isLoading: false,
+      touched: { password: true }
+    });
+
+    const passwordInput = component.find('TextInput[field="password"]').at(0);
+
+    expect(passwordInput.props().error).toBeFalsy();
+  });
+
+  it('should render correct props when touched and there are errors', async () => {
+    const { component } = setupShallow({
+      isLoading: false,
+      touched: { password: true },
+      errors: { password: 'Error message' }
+    });
+
+    const passwordInput = component.find('TextInput[field="password"]').at(0);
+
+    expect(passwordInput.props().error).toEqual('Error message');
+  });
+});
