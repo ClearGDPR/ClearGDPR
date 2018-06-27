3a48c0377f5ed98e4b763b044a669ae8d366ef18
Francisco Caballero <fjcero@gmail.com>
2018-06-27 01:28:39 -0300

    Updated dashboard processors to use data factory

--- admin/src/components/core/pages/Dashboard.js
+++ admin/src/components/core/pages/Dashboard.js
@@ -9,36 +9,9 @@ import Loader from './../cards/dashboard/Loader';
 import Form from './../cards/dashboard/Form';
 import './dashboard.css';
 
-import nielsenIMG from '../../../assets/images/processors/nielsen.svg';
-import liverampIMG from '../../../assets/images/processors/liveramp.svg';
-import experianIMG from '../../../assets/images/processors/experian.svg';
+import * as ProcessorsDataFactory from '../../../tests/data/processors.factory';
 
-let lorem =
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum placeat neque, pariatur ipsa qui facere nobis error iste consectetur quo exercitationem!';
-
-export const processors = [
-  {
-    name: 'Nielsen',
-    img: nielsenIMG,
-    consented: 72,
-    description: lorem,
-    scope: ['Full Name', 'Email', 'Phone Number']
-  },
-  {
-    name: 'Live Ramp',
-    img: liverampIMG,
-    consented: 64,
-    description: lorem,
-    scope: ['Email']
-  },
-  {
-    name: 'Experian',
-    img: experianIMG,
-    consented: 25,
-    description: lorem,
-    scope: ['Full Name', 'Phone Number']
-  }
-];
+export const processors = ProcessorsDataFactory.getAll();
 
 export const users = [
   {
