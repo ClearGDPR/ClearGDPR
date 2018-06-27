1fe76f20d0c85b8b9ba568860dddd73d42868192
Francisco Caballero <fjcero@gmail.com>
2018-06-27 13:18:42 -0300

    Replace uses of flushPromises with new path

--- admin/src/containers/Users/ChangePassword.test.js
+++ admin/src/containers/Users/ChangePassword.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import session from '../../helpers/Session';
-import TestUtils from '../../helpers/TestUtils';
+import * as TestUtils from '../../tests/helpers/TestUtils';
 
 import { ChangePasswordContainer as ChangePassword } from './ChangePassword';
 
