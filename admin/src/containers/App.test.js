82c78bb44006da802da33a1a905745750671c99b
Jake Davies <jgdavies24@gmail.com>
2018-07-02 11:21:53 -0700

    test without localStorage fixed

--- admin/src/containers/App.test.js
+++ admin/src/containers/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import localStorage from 'node-localstorage';
 
 it('renders without crashing', () => {
+  global.localStorage = new localStorage.LocalStorage('');
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
