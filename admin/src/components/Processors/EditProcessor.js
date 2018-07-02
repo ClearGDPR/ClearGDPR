c01d65e4ef6e618e81ffc90e6de3863c7bfd4107
Francisco Caballero <fjcero@gmail.com>
2018-07-02 11:51:32 -0300

    Updated processors form test snapshots

--- admin/src/components/Processors/EditProcessor.js
+++ admin/src/components/Processors/EditProcessor.js
@@ -1,45 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Text, TextArea, Checkbox } from 'react-form';
+import { Form, TextArea, Checkbox } from 'react-form';
 
+import TextInput from '../core/cards/dashboard/TextInput';
 import Loader from '../core/cards/dashboard/Loader';
 
-export const EditProcessor = props => {
-  const scopes = {
-    'Full Name': 'user.fullName',
-    Email: 'user.email',
-    'Phone Number': 'user.phoneNumber'
+// TODO: this should be fetch from configuration service
+const DEMO_SCOPES = {
+  'Full Name': 'user.fullName',
+  Email: 'user.email',
+  'Phone Number': 'user.phoneNumber'
+};
+
+export class EditProcessor extends React.Component {
+  static propTypes = {
+    isLoading: PropTypes.bool,
+    onSubmit: PropTypes.func,
+    touched: PropTypes.object,
+    errors: PropTypes.object
   };
 
-  const renderForm = () => (
-    <React.Fragment>
-      <label htmlFor="name">Name:</label>
-      <Text field="name" id="name" />
+  renderForm() {
+    return (
+      <React.Fragment>
+        <TextInput
+          label="Name"
+          placeholder="*********"
+          type={'text'}
+          error={
+            this.props.touched &&
+            this.props.touched['name'] &&
+            this.props.errors &&
+            this.props.errors['name']
+          }
+          field="name"
+          required
+        />
 
-      <label htmlFor="description">Description:</label>
-      <TextArea field="description" id="description" />
+        <label htmlFor="description">Description:</label>
+        <TextArea field="description" id="description" />
 
-      <label htmlFor="logoUrl">Logo URL:</label>
-      <Text field="logoUrl" id="logoUrl" />
+        <TextInput
+          label="Logo URL"
+          placeholder="*********"
+          type={'text'}
+          error={
+            this.props.touched &&
+            this.props.touched['logoUrl'] &&
+            this.props.errors &&
+            this.props.errors['logoUrl']
+          }
+          field="logoUrl"
+          required
+        />
 
-      <label>Scopes:</label>
-      {Object.keys(scopes).map((scope, i) => <Checkbox key={i} field={`scope[${scope}]`} />)}
+        <label>Scopes:</label>
+        {Object.keys(DEMO_SCOPES).map((scope, i) => <Checkbox key={i} field={`scope[${scope}]`} />)}
 
-      <button type="submit" className="btn">
-        Submit
-      </button>
-    </React.Fragment>
-  );
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </React.Fragment>
+    );
+  }
 
-  return <form onSubmit={props.onSubmit}>{props.isLoading ? <Loader /> : renderForm()}</form>;
-};
-
-EditProcessor.propTypes = {
-  isLoading: PropTypes.bool,
-  onSubmit: PropTypes.func,
-  touched: PropTypes.object,
-  errors: PropTypes.object
-};
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+        {this.props.isLoading ? <Loader /> : this.renderForm()}
+      </form>
+    );
+  }
+}
 
 const EditProcessorForm = props => (
   <Form onSubmit={submittedValues => props.onSubmit && props.onSubmit(submittedValues)}>
--- admin/src/components/Processors/EditProcessor.test.js
+++ admin/src/components/Processors/EditProcessor.test.js
@@ -1,41 +1,35 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
-import EditProcessorForm, { EditProcessor } from './EditProcessor';
+import { EditProcessor } from './EditProcessor';
 
-const setup = propOverrides => {
+const setupShallow = propOverrides => {
   const props = Object.assign({}, propOverrides);
   const component = shallow(<EditProcessor {...props} />);
-  const mountedComponent = mount(<EditProcessorForm {...props} />);
 
-  return {
-    props,
-    component,
-    mountedComponent
-  };
+  return { props, component };
 };
 
-describe('(Component) Edit Processor form', () => {
+describe('(Component) Edit Processor Form', () => {
   it('should render correctly when no props provided', async () => {
-    const { component } = setup();
+    const { component } = setupShallow();
     expect(component).toMatchSnapshot();
   });
 
   it('should render correctly when the prop isLoading is set to false', async () => {
-    const { component } = setup({ isLoading: false });
+    const { component } = setupShallow({ isLoading: false });
     expect(component).toMatchSnapshot();
   });
 
   it('should render correctly when the prop isLoading is set to true', async () => {
-    const { component } = setup({ isLoading: true });
+    const { component } = setupShallow({ isLoading: true });
     expect(component).toMatchSnapshot();
   });
 
   it('should call onSubmit when submit button clicked', async () => {
     const onSubmit = jest.fn();
-    const { component } = setup({ onSubmit });
+    const { component } = setupShallow({ onSubmit });
 
-    // TODO: validation on fields should be triggered
     const form = component.find('form').at(0);
     form.simulate('submit');
     expect(onSubmit).toHaveBeenCalled();
