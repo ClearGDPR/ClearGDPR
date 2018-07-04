9bbac252198eaede36353f1451fc08622ff012ed
Francisco Caballero <fjcero@gmail.com>
2018-07-04 11:34:45 -0300

    Added types to babelrc

--- admin/src/types/index.js
+++ admin/src/types/index.js
@@ -3,7 +3,7 @@ import { shape, number, string, arrayOf } from 'prop-types';
 export const processorType = shape({
   id: number,
   name: string,
-  logoUrl: description,
+  description: string,
   logoUrl: string,
   scopes: arrayOf(string)
 });
