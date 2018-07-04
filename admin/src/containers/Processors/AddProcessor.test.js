48cc4a6d15cfd64155a84cfd11adf3a894e565bc
Francisco Caballero <fjcero@gmail.com>
2018-07-03 22:52:26 -0300

    Updated processor form after refactor.

---
+++ admin/src/containers/Processors/AddProcessor.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import * as TestUtils from 'tests/helpers/TestUtils';
+
+import { AddProcessorContainer } from './AddProcessor';
+
+const setupShallow = propOverrides => {
+  const props = Object.assign(
+    {
+      addProcessor: jest.fn().mockReturnValue(Promise.resolve()),
+      closePanel: jest.fn(),
+      isLoading: false
+    },
+    propOverrides
+  );
+  const component = shallow(<AddProcessorContainer {...props} />);
+
+  return { props, component };
+};
+
+describe('(Container) Add Processor', () => {
+  it('should render correctly when default props provided', async () => {
+    const { component } = setupShallow();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should populate errors on submit when create processor rejects with error', async () => {
+    const { props, component } = setupShallow();
+    props.addProcessor.mockReturnValue(Promise.reject(new Error('Test error')));
+    component.instance().onSubmit({ name: null, logoUrl: null, scopes: [] });
+
+    await TestUtils.flushPromises();
+    expect(component.state()).toMatchSnapshot();
+  });
+
+  it('should close panel when registration succeeded', async () => {
+    const { props, component } = setupShallow();
+    component
+      .instance()
+      .onSubmit({ name: 'Processor Name', logoUrl: 'https://a_valid_url.com/file.png', scopes: [] });
+
+    await TestUtils.flushPromises();
+    expect(props.closePanel).toHaveBeenCalled();
+  });
+});
