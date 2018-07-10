86bfacfee982f77b8085ef0a6dce5feb8b075bb0
Francisco Caballero <fjcero@gmail.com>
2018-07-10 13:09:03 -0300

    Updated kitchen sink overview

--- admin/src/components/core/cards/dashboard/TableCard.js
+++ admin/src/components/core/cards/dashboard/TableCard.js
@@ -2,22 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Card from './Card';
-import Table from 'components/core/Common/Table';
+import Table from 'components/core/Common/Table/Table';
 
 const TableCard = props => {
   return (
-    <Card cols={12} title={props.title}>
+    <Card cols={props.cols} title={props.title}>
       <div className="content">
-        <Table rows={props.rows} columns={props.header} actions={props.actions} />
+        <Table rows={props.rows} columns={props.columns} actions={props.actions} />
       </div>
     </Card>
   );
 };
 
 TableCard.propTypes = {
-  header: PropTypes.array,
+  cols: PropTypes.number,
   title: PropTypes.string,
-  rows: PropTypes.array,
+  rows: PropTypes.array.isRequired,
+  columns: PropTypes.object.isRequired,
   actions: PropTypes.array
 };
 
