606c53aa348e755647e0f17853f8f9a4b51dc991
Francisco Caballero <fjcero@gmail.com>
2018-07-10 11:17:00 -0300

    Added users data factory

---
+++ admin/src/tests/data/users.factory.js
@@ -0,0 +1,22 @@
+// TODO: Use https://github.com/marak/Faker.js/
+
+export const users = [
+  {
+    id: 1,
+    username: 'admin'
+  },
+  {
+    id: 2,
+    username: 'joe'
+  },
+  {
+    id: 3,
+    username: 'marc'
+  },
+  {
+    id: 4,
+    username: 'gina'
+  }
+];
+
+export const getAll = () => users;
