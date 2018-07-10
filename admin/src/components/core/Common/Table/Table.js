8cdc777f0c726f49fd95addcfdda1a2cc92767f0
Francisco Caballero <fjcero@gmail.com>
2018-07-10 12:55:17 -0300

    Filter columns given a filtering function

--- admin/src/components/core/Common/Table/Table.js
+++ admin/src/components/core/Common/Table/Table.js
@@ -2,32 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Table.css';
 
-const Table = ({ rows, columns, actions }) => (
-  <table className="responsive-table">
-    <thead>
-      <tr>
-        {Object.keys(columns).map(key => <th key={key}>{columns[key].title}</th>)}
-        {actions.length && <th>Actions</th>}
-      </tr>
-    </thead>
-    <tbody>
-      {rows.map((item, i) => (
-        <tr key={i}>
-          {Object.keys(columns).map(key => <td key={key}>{item[key]}</td>)}
-          {actions.length && (
-            <td>
-              {actions.map((action, i) => (
-                <button key={i} className="ui-action btn" onClick={e => action.onClick(e, item)}>
-                  {action.label}
-                </button>
-              ))}
-            </td>
-          )}
+const Table = ({ rows, columns, actions }) => {
+  const tableColums = Object.keys(columns)
+    .filter(col => columns[col].filter())
+    .reduce((cols, key) => {
+      return {
+        ...cols,
+        [key]: columns[key]
+      };
+    }, {});
+
+  return (
+    <table className="responsive-table">
+      <thead>
+        <tr>
+          {Object.keys(tableColums).map(key => <th key={key}>{columns[key].title}</th>)}
+          {actions.length && <th>Actions</th>}
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {rows.map((item, i) => (
+          <tr key={i}>
+            {Object.keys(tableColums).map(key => <td key={key}>{item[key]}</td>)}
+            {actions.length && (
+              <td>
+                {actions.map((action, i) => (
+                  <button key={i} className="ui-action btn" onClick={e => action.onClick(e, item)}>
+                    {action.label}
+                  </button>
+                ))}
+              </td>
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 Table.propTypes = {
   rows: PropTypes.arrayOf(PropTypes.object),
