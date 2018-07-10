5cc4bd60a610ff7559638ad6fcde41b9b5f19931
Francisco Caballero <fjcero@gmail.com>
2018-07-10 12:43:03 -0300

    Added Actions to Table component

--- admin/src/components/core/Common/Table/Table.js
+++ admin/src/components/core/Common/Table/Table.js
@@ -2,22 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Table.css';
 
-const Table = ({ items, columns }) => (
+const Table = ({ rows, columns, actions }) => (
   <table className="responsive-table">
     <thead>
-      <tr>{Object.keys(columns).map(key => <th key={key}>{columns[key].title}</th>)}</tr>
+      <tr>
+        {Object.keys(columns).map(key => <th key={key}>{columns[key].title}</th>)}
+        {actions.length && <th>Actions</th>}
+      </tr>
     </thead>
     <tbody>
-      {items.map((item, i) => (
-        <tr key={i}>{Object.keys(columns).map(key => <td key={key}>{item[key]}</td>)}</tr>
+      {rows.map((item, i) => (
+        <tr key={i}>
+          {Object.keys(columns).map(key => <td key={key}>{item[key]}</td>)}
+          {actions.length && (
+            <td>
+              {actions.map((action, i) => (
+                <button key={i} className="ui-action btn" onClick={e => action.onClick(e, item)}>
+                  {action.label}
+                </button>
+              ))}
+            </td>
+          )}
+        </tr>
       ))}
     </tbody>
   </table>
 );
 
 Table.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.object).isRequired,
-  columns: PropTypes.object.isRequired
+  rows: PropTypes.arrayOf(PropTypes.object),
+  columns: PropTypes.object.isRequired,
+  actions: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default Table;
