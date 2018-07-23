2395824905d09ee6706159fc5a5b3a22f1b00bc0
Francisco Caballero <fjcero@gmail.com>
2018-07-23 17:22:45 -0300

    Revert change on generate-config

--- frontend/src/Elements/components/Consent/Consent.test.js
+++ frontend/src/Elements/components/Consent/Consent.test.js
@@ -3,20 +3,49 @@ import { shallow } from 'enzyme';
 import Consent from './Consent';
 
 const processors = [
-
+  {
+    name: 'Nielsen',
+    logoUrl: '',
+    consented: 72,
+    description: '',
+    scopes: ['Full Name', 'Email', 'Phone Number']
+  },
+  {
+    name: 'Live Ramp',
+    logoUrl: '',
+    consented: 64,
+    description: '',
+    scopes: ['Email']
+  },
+  {
+    name: 'Experian',
+    logoUrl: '',
+    consented: 25,
+    description: '',
+    scopes: ['Full Name', 'Phone Number']
+  }
 ];
 
 const setup = propOverrides => {
   const props = {
-    processors
+    processors,
+    ...propOverrides
   };
-  
+
   const component = shallow(<Consent {...props} />);
 
   return { props, component };
-}
+};
 
 describe('(Elements) Consent', () => {
-  it('should do the math correctly', () => {
+  beforeAll(() => {
+    window.cg = {
+      Subject: {
+        giveConsent: () => {},
+        getProcessors: () => {}
+      }
+    };
   });
+
+  it('should render correctly', () => {});
 });
