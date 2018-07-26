e42304f774d1828ca7d92449bf19cb0a95ed9094
Dmytro Zasiadko <segoddnja@gmail.com>
2018-07-26 16:33:49 +0200

    listen to changes of the token in local storage and prepare application for new subject session

--- frontend/src/containers/SubjectContext.js
+++ frontend/src/containers/SubjectContext.js
@@ -3,6 +3,39 @@ import PropTypes from 'prop-types';
 
 const SubjectContext = React.createContext();
 
+const DEFAULT_STATE = {
+  isFetched: false,
+  isLoading: false,
+  isErased: false,
+  data: null,
+  status: null
+};
+
+let originalLocalStorageSetItem;
+const EVENT_NAME = 'localStorage.itemInserted';
+const LOCAL_STORAGE_EVENTS = [EVENT_NAME, 'storage'];
+
+function upgradeLocalStorage() {
+  if (originalLocalStorageSetItem) {
+    return;
+  }
+
+  originalLocalStorageSetItem = localStorage.setItem;
+  localStorage.setItem = function() {
+    originalLocalStorageSetItem.apply(this, arguments);
+
+    const keyboardEvent = new CustomEvent(EVENT_NAME);
+    keyboardEvent.key = arguments[0];
+    keyboardEvent.value = arguments[1];
+    window.dispatchEvent(keyboardEvent);
+  };
+}
+
+function downgradeLocalStorage() {
+  localStorage.setItem = originalLocalStorageSetItem;
+  originalLocalStorageSetItem = null;
+}
+
 export class SubjectProvider extends React.Component {
   static propTypes = {
     children: PropTypes.oneOfType([
@@ -12,13 +45,13 @@ export class SubjectProvider extends React.Component {
     ])
   };
 
-  fetchData = async () => {
+  _fetchData = async () => {
     const { isLoading, isFetched, isGuest } = this.state;
     if (isLoading || isFetched || isGuest) {
       return;
     }
 
-    this.setLoading(true);
+    this._setLoading(true);
 
     const cgToken = localStorage.getItem('cgToken');
 
@@ -52,19 +85,47 @@ export class SubjectProvider extends React.Component {
     });
   };
 
-  setLoading() {
+  _setLoading() {
     this.setState({ isLoading: true });
   }
 
-  state = {
-    isGuest: !localStorage.getItem('cgToken'),
-    fetchData: this.fetchData,
-    isLoading: false,
-    isFetched: false,
-    isErased: false,
-    data: null,
-    status: null
-  };
+  _localStorageEventListener(e) {
+    const { key } = e;
+    if (key && key !== 'cgToken') {
+      return;
+    }
+
+    this.setState(this._makeNewSubjectSession());
+    console.log(this.state);
+  }
+
+  _makeNewSubjectSession() {
+    return {
+      ...DEFAULT_STATE,
+      fetchData: this._fetchData,
+      isGuest: !localStorage.getItem('cgToken')
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = this._makeNewSubjectSession();
+    this._localStorageEventListener = this._localStorageEventListener.bind(this);
+  }
+
+  componentDidMount() {
+    upgradeLocalStorage();
+    LOCAL_STORAGE_EVENTS.map(name =>
+      window.addEventListener(name, this._localStorageEventListener)
+    );
+  }
+
+  componentWillUnmount() {
+    LOCAL_STORAGE_EVENTS.map(name =>
+      window.removeEventListener(name, this._localStorageEventListener)
+    );
+    downgradeLocalStorage();
+  }
 
   render() {
     return (
