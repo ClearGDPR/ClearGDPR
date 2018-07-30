532b1fc7ef93019d3f2dd30c723a69cd934fba14
Dmytro Zasiadko <segoddnja@gmail.com>
2018-07-30 16:50:37 +0200

    Connect to websocket and listen for events
    closes #320

--- admin/src/components/Dashboard/Widgets/Event.js
+++ admin/src/components/Dashboard/Widgets/Event.js
@@ -1,30 +1,17 @@
 import React from 'react';
 
-export default event => (
-  <React.Fragment>
-    <p>
-      <b>Event name</b> received from <b>Node name:</b>
-    </p>
-    <ul>
-      <li>address: String, 32 Bytes - address from which this log originated.</li>
-      <li>args: Object - The arguments coming from the event.</li>
-      <li>
-        blockHash: String, 32 Bytes - hash of the block where this log was in. null when its
-        pending.
-      </li>
-      <li>blockNumber: Number - the block number where this log was in. null when its pending.</li>
-      <li>logIndex: Number - integer of the log index position in the block.</li>
-      <li>event: String - The event name.</li>
-      <li>
-        removed: bool - indicate if the transaction this event was created from was removed from the
-        blockchain (due to orphaned block) or never get to it (due to rejected transaction).
-      </li>
-      <li>
-        transactionIndex: Number - integer of the transactions index position log was created from.
-      </li>
-      <li>
-        transactionHash: String, 32 Bytes - hash of the transactions this log was created from.
-      </li>
-    </ul>
-  </React.Fragment>
-);
+export default ({ event }) => {
+  const { eventName, from, fromName } = event;
+
+  return (
+    <React.Fragment>
+      <p>
+        <b>{eventName}</b> received from <b>{fromName}</b>:
+      </p>
+      <ul>
+        <li>address: {from}</li>
+        <li>args: Object - The arguments coming from the event.</li>
+      </ul>
+    </React.Fragment>
+  );
+};
