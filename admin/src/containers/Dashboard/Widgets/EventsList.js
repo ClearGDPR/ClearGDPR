c5592d22fb05e61b4921a364f6ee212a1fb5ac98
Dmytro Zasiadko <segoddnja@gmail.com>
2018-07-30 13:03:20 +0200

    add boilerplate admin events list component and container

---
+++ admin/src/containers/Dashboard/Widgets/EventsList.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import config from 'config';
+import SubjectsListComponent from 'components/Dashboard/Widgets/EventsList';
+import internalFetch from 'helpers/internal-fetch';
+import Paginate from 'components/core/Paginate';
+
+export class EventsListContainer extends React.Component {
+  state = {
+    isLoading: false,
+    errorState: false,
+    paging: {},
+    subjects: []
+  };
+
+  fetchSubjects(page = 1) {
+    this.setState({ errorState: false, loading: true });
+    internalFetch(`${config.API_URL}/api/management/subjects/list?page=${page}`)
+      .then(({ data, paging }) => {
+        this.setState({ subjects: data, paging, loading: false });
+      })
+      .catch(() => {
+        this.setState({ errorState: true, loading: false });
+      });
+  }
+
+  componentDidMount() {
+    //this.fetchSubjects();
+  }
+
+  handlePageClick = data => {
+    const page = data.selected;
+    this.fetchSubjects(page + 1);
+  };
+
+  render() {
+    return (
+      <div>
+        <SubjectsListComponent {...this.state} />
+        <Paginate
+          pageCount={(this.state.paging && this.state.paging.total) || 1}
+          onPageChange={this.handlePageClick}
+        />
+      </div>
+    );
+  }
+}
+
+export default EventsListContainer;
