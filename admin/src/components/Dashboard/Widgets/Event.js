b7a79fdfb8900492de45c80b650e0cd278b767f2
Dmytro Zasiadko <segoddnja@gmail.com>
2018-08-06 13:59:30 +0200

    show web socket connection status implement different views for different events

--- admin/src/components/Dashboard/Widgets/Event.js
+++ admin/src/components/Dashboard/Widgets/Event.js
@@ -1,8 +1,90 @@
+// findsecrets-ignore-file
 import React from 'react';
+
+const ProcessorsUpdated = ({ params = {} }) => (
+  <React.Fragment>New processor address - {params.address}</React.Fragment>
+);
+
+const ConsentGiven = ({ params = {} }) => (
+  <ul>
+    <li>Given by subject with id {params.subjectId}</li>
+    <li>Givent to processors with id {params.processorsConsented.join(', ')}</li>
+  </ul>
+);
+
+const DataAccessed = ({ params = {} }) => {
+  return <React.Fragment>Data of subject with id {params.subjectId}</React.Fragment>;
+};
+
+const DataRectified = ({ params = {} }) => {
+  return (
+    <React.Fragment>
+      Data of subject with id {params.subjectId} rectified. Rectifications count is{' '}
+      {params.rectificationCount}
+    </React.Fragment>
+  );
+};
+
+const DataRestricted = ({ params = {} }) => {
+  return (
+    <React.Fragment>Data processing restricted by subject id {params.subjectId}.</React.Fragment>
+  );
+};
+
+const DataObjected = ({ params = {} }) => {
+  return (
+    <React.Fragment>Data processing objected by subject id {params.subjectId}.</React.Fragment>
+  );
+};
+
+const DataErasedByController = ({ params = {} }) => {
+  return (
+    <React.Fragment>
+      Subjects data(subject id {params.subjectId}) erased by controller.
+    </React.Fragment>
+  );
+};
+
+const DataErasedByProcessor = ({ params = {} }) => {
+  return (
+    <React.Fragment>
+      Subjects data(subject id {params.subjectId}) erased by processor address {params.processor}.
+    </React.Fragment>
+  );
+};
+
+const GenericEvent = ({ params = {} }) => {
+  const argumentsList = Object.keys(params).map(argument => (
+    <li key={argument}>
+      {argument} => {params[argument]}
+    </li>
+  ));
+
+  return (
+    <React.Fragment>
+      {argumentsList.length > 0 ? (
+        <React.Fragment>
+          Event arguments:<ul>{argumentsList}</ul>
+        </React.Fragment>
+      ) : null}
+    </React.Fragment>
+  );
+};
+
+const EVENTS_MAP = {
+  Controller_ProcessorsUpdated: ProcessorsUpdated,
+  Controller_ConsentGivenTo: ConsentGiven,
+  Controller_SubjectDataAccessed: DataAccessed,
+  Controller_SubjectDataRectified: DataRectified,
+  Controller_SubjectDataRestricted: DataRestricted,
+  Controller_SubjectDataObjected: DataObjected,
+  Controller_SubjectDataErased: DataErasedByController,
+  Processor_SubjectDataErased: DataErasedByProcessor
+};
 
 export default ({ event }) => {
   const { eventName, from, fromName } = event;
-
+  const eventView = EVENTS_MAP[eventName] || GenericEvent;
   return (
     <React.Fragment>
       <p>
@@ -10,7 +92,7 @@ export default ({ event }) => {
       </p>
       <ul>
         <li>address: {from}</li>
-        <li>args: Object - The arguments coming from the event.</li>
+        <li>{React.createElement(eventView, event)}</li>
       </ul>
     </React.Fragment>
   );
--- admin/src/components/Dashboard/Widgets/EventsList.js
+++ admin/src/components/Dashboard/Widgets/EventsList.js
@@ -4,25 +4,32 @@ import format from 'helpers/date-formatter';
 import TableCard from 'components/core/cards/dashboard/TableCard';
 import Event from './Event';
 
-const EventsList = ({ events, isLoading, errorState }) => {
+const EventsList = ({ events, connected, errorState }) => {
   const columns = {
     event: { title: 'Event' },
     date: { title: 'Date' }
   };
-
-  // TODO: loading?1
 
   const rows = events.map(eventData => ({
     event: <Event event={eventData} />,
     date: format(eventData.time)
   }));
 
-  return <TableCard columns={columns} rows={rows} title="Events" />;
+  let tag;
+  if (connected) {
+    tag = <span className="tag is-info">Connected</span>;
+  } else if (errorState) {
+    tag = <span className="tag is-danger">Connection error</span>;
+  } else {
+    tag = <span className="tag is-light">Disconnected</span>;
+  }
+
+  return <TableCard columns={columns} rows={rows} title={<p>Events {tag}</p>} />;
 };
 
 EventsList.propTypes = {
   events: PropTypes.array,
-  isLoading: PropTypes.bool,
+  connected: PropTypes.bool,
   errorState: PropTypes.bool
 };
 
