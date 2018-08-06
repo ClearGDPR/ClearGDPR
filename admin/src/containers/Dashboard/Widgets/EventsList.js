27d2e265b96ef2bf5c67b728cba12e899b224f16
Dmytro Zasiadko <segoddnja@gmail.com>
2018-08-06 13:59:30 +0200

    show web socket connection status implement different views for different events

--- admin/src/containers/Dashboard/Widgets/EventsList.js
+++ admin/src/containers/Dashboard/Widgets/EventsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import config from 'config';
+import config from 'config';
 import SubjectsListComponent from 'components/Dashboard/Widgets/EventsList';
 import Paginate from 'components/core/Paginate';
 
@@ -8,21 +8,29 @@ export class EventsListContainer extends React.Component {
     isLoading: false,
     errorState: false,
     paging: {},
-    events: [
-      JSON.parse(
-        '{"params":{"0":"0x3b4066bd7b7960752225af105d3beafb5c47a26c5aae7e6798a437b7c0bb33e6","1":["0x9954CC7e418D3D1833B2971711eb068D083a4166","0x78243779b6c9598392a87869e7cb76fa799D938C"],"subjectId":"0x3b4066bd7b7960752225af105d3beafb5c47a26c5aae7e6798a437b7c0bb33e6","processorsConsented":["0x9954CC7e418D3D1833B2971711eb068D083a4166","0x78243779b6c9598392a87869e7cb76fa799D938C"]},"eventName":"Controller_ConsentGivenTo","from":"0x9954CC7e418D3D1833B2971711eb068D083a4166","time":1532960063175,"fromName":"Master Controller Node"}'
-      )
-    ]
+    events: []
   };
 
   componentDidMount() {
-    this.ws = new WebSocket('ws://localhost:8082/api/management/events/feed');
-
-    this.ws.onmessage = event => {
-      const eventData = JSON.parse(event.data);
-      console.log(eventData);
-      this.setState({ events: this.state.events.concat([eventData]) });
+    this.websocket = new WebSocket(
+      config.API_URL.replace('http://', 'ws://') + '/api/management/events/feed'
+    );
+    this.websocket.onopen = event => {
+      this.setState({ connected: true });
     };
+    this.websocket.onmessage = event => {
+      this._addEvent(JSON.parse(event.data));
+    };
+    this.websocket.onclose = () => {
+      this.setState({ connected: false });
+    };
+    this.websocket.onerror = event => {
+      this.setState({ errorState: true });
+    };
+  }
+
+  _addEvent(event) {
+    this.setState({ events: this.state.events.concat([event]) });
   }
 
   componentWillUnmount() {
