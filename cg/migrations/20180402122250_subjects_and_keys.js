78d29c8239a2329fbce6b3899f076c7ba415744b
Dmytro Zasiadko <dmytro@clevertech.biz>
2018-08-28 15:15:29 +0300

    clean migrations closes #420

--- cg/migrations/20180402122250_subjects_and_keys.js
+++ cg/migrations/20180402122250_subjects_and_keys.js
@@ -7,6 +7,22 @@ exports.up = function(knex, Promise) {
         .primary();
       table.timestamps(false, true);
       table.text('personal_data').notNullable();
+      table
+        .boolean('direct_marketing')
+        .defaultTo(false)
+        .notNullable();
+      table
+        .boolean('email_communication')
+        .defaultTo(false)
+        .notNullable();
+      table
+        .boolean('research')
+        .defaultTo(false)
+        .notNullable();
+      table
+        .boolean('objection')
+        .defaultTo(false)
+        .notNullable();
     }),
     knex.schema.createTable('subject_keys', table => {
       table
--- cg/migrations/20180427134153_processor_address.js
+++ cg/migrations/20180427134153_processor_address.js
@@ -2,8 +2,11 @@ exports.up = function(knex) {
   return knex.schema.createTable('processor_address', table => {
     table.integer('processor_id');
     table.string('address', 42);
-    table.foreign('processor_id').references('processors.id');
     table.primary(['processor_id']);
+    table
+      .foreign('processor_id')
+      .references('processors.id')
+      .onDelete('CASCADE');
   });
 };
 
