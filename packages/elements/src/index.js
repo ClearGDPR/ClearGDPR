b0b63a00dcf3d1e7b7a106dcf2f04a3d20b90acc
Francisco Caballero <fjcero@gmail.com>
2018-08-29 05:49:13 -0300

    Updated build. Export modules properly.

--- packages/elements/src/index.js
+++ packages/elements/src/index.js
@@ -1,3 +1,5 @@
-import Elements from './Elements';
-
-export default Elements;
+export { default as Element } from './Element';
+export { default as Elements } from './Elements';
+export { default as Subject } from './contexts/Subject';
+export { default as ConsentSuccess } from './components/Consent/ConsentSuccess';
+export { SubjectProvider, inject } from './contexts/SubjectContext';
