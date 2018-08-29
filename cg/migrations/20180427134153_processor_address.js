15c97f0041e97471f7eeb08cd3449d2f67fca621
sindelio <sindelio@gmail.com>
2018-08-29 18:16:34 -0300

    Some comments for the future

--- cg/migrations/20180427134153_processor_address.js
+++ cg/migrations/20180427134153_processor_address.js
@@ -1,12 +1,13 @@
 exports.up = function(knex) {
   return knex.schema.createTable('processor_address', table => {
-    table.integer('processor_id');
-    table.string('address', 42);
+    table.integer('processor_id'); // I don't need this if I specify the primary below
+    table.string('address', 42); // account address, unique
     table.primary(['processor_id']);
     table
       .foreign('processor_id')
       .references('processors.id')
-      .onDelete('CASCADE');
+      .onDelete('CASCADE'); // What exactly this cascade means?
+    // enode
   });
 };
 
